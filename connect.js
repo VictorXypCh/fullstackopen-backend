ad7938c83a30c3199366088722c8c27a01c4dba9
VictorXypCh <victorxyp.ch@gmail.com>
2023-02-13 17:08:30 +0700

    Add MongoMemoryServer for testing

--- connect.js
+++ connect.js
@@ -1,23 +1,34 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 
-const MONGODB_URI =
+const { MongoMemoryServer } = require("mongodb-memory-server");
+
+let MONGODB_URI =
   process.env.NODE_ENV === "test"
     ? process.env.TEST_MONGODB_URI
     : process.env.MONGODB_URI;
-
+// This will create an new instance of "MongoMemoryServer" and automatically start it
 mongoose.set("strictQuery", false);
+let mongod;
+const connect = async () => {
+  if (process.env.NODE_ENV === "test") {
+    mongod = await MongoMemoryServer.create();
+    console.log("** Started MongoMemoryServer **");
+    MONGODB_URI = mongod.getUri();
+  }
 
-const connect = () => {
   mongoose
     .connect(MONGODB_URI)
     .then(() => {
       console.log("** connect MongoDB **");
+      console.log(MONGODB_URI);
     })
     .catch((err) => {
       console.log(err);
     });
 };
+
 module.exports = {
   connect,
-}
+    mongod
+};
--- index.js
+++ index.js
@@ -2,6 +2,17 @@ const app = require("./app"); // the actual Express application
 const config = require("./utils/config");
 const logger = require("./utils/logger");
 
-app.listen(config.PORT, () => {
+const server = app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`);
 });
+
+// Stop MongoMemoryServer server when stop server in test mode
+if (process.env.NODE_ENV === "test") {
+  process.on("SIGTERM", async () => {
+    await require("./connect").mongod.stop();
+    debug("SIGTERM signal received: closing HTTP server");
+    server.close(() => {
+      debug("HTTP server closed");
+    });
+  });
+}
