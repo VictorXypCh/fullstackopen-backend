02141f179d203c81f99a186c9c311d102bde3b28
chatlitbooks <chatlitbooks@gmail.com>
2023-02-13 09:05:40 +0700

    test

--- connect.js
+++ connect.js
@@ -1,20 +1,23 @@
-require("dotenv").config()
-const mongoose = require("mongoose")
+require("dotenv").config();
+const mongoose = require("mongoose");
 
-const url = process.env.MONGODB_URI
+const MONGODB_URI =
+  process.env.NODE_ENV === "test"
+    ? process.env.TEST_MONGODB_URI
+    : process.env.MONGODB_URI;
 
-mongoose.set("strictQuery", false)
+mongoose.set("strictQuery", false);
 
 const connect = () => {
   mongoose
-    .connect(url)
+    .connect(MONGODB_URI)
     .then(() => {
-      console.log("** connect MongoDB **")
+      console.log("** connect MongoDB **");
     })
     .catch((err) => {
-      console.log(err)
-    })
-}
+      console.log(err);
+    });
+};
 module.exports = {
   connect,
 }
