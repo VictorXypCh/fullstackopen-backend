7003d2386d4ff824f04f15b8df0f23a8c01df6c7
VictorXypCh <victorxyp.ch@gmail.com>
2023-02-15 14:07:30 +0700

    4.13-4.14 blog list can be updated(done) | complete part 4: b. Testing the backend

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,28 +1,41 @@
+const route = require("express").Router();
+const Blog = require("../models/Blog");
+route.get("/", (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs);
+  });
+});
+route.get("/:id", async (request, response) => {
+  const result = await Blog.findById(request.params.id);
+  response.json(result.toJSON());
+});
 
-const route = require("express").Router()
-const Blog = require('../models/Blog')
-route.get('/', (request, response) => {
-    Blog
-        .find({})
-        .then(blogs => {
-            response.json(blogs)
-        })
-})
+route.post("/", (request, response) => {
+  const blog = new Blog(request.body);
 
-route.post('/', (request, response) => {
-    const blog = new Blog(request.body)
+  blog.save().then((result) => {
+    response.status(201).json(result);
+  });
+});
 
-    blog
-        .save()
-        .then(result => {
-            response.status(201).json(result)
-        })
-})
+route.put("/:id", async (request, response) => {
+  const body = request.body;
+  const updateObj = {
+    title: body.title,
+    author: body.author,
+    url: !body.url ? "" : body.url,
+    likes: body.likes,
+  };
+  const result = await Blog.findByIdAndUpdate(request.params.id, updateObj, {
+    new: true,
+  });
+  response.json(result);
+});
 
-route.delete('/:id', async (request, response) => {
-    await Blog.findOneAndRemove(request.params.id);
-    response.status(204).end();
-})
+route.delete("/:id", async (request, response) => {
+  await Blog.findOneAndRemove(request.params.id);
+  response.status(204).end();
+});
 
+module.exports = route;
 
-module.exports = route
--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,64 +1,70 @@
-
-const supertest = require('supertest')
-const mongoose = require('mongoose')
-const app = require('../app')
-const Blog = require('../models/Blog')
-const api = supertest(app)
-
-
+const supertest = require("supertest");
+const mongoose = require("mongoose");
+const app = require("../app");
+const Blog = require("../models/Blog");
+const api = supertest(app);
 
 const initial = [
-    {
-        _id: "5a422aa71b54a676234d17f8",
-        title: "Go To Statement Considered Harmful",
-        author: "Edsger W. Dijkstra",
-        url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-        likes: 5,
-        __v: 0,
-    },
-    {
-        title: "Canonical string reduction",
-        author: "Edsger W. Dijkstra",
-        likes: 12,
-    },
-    {
-        author: "Robert C. Martin",
-        blogs: 3,
-    },
+  {
+    _id: "5a422aa71b54a676234d17f8",
+    title: "Go To Statement Considered Harmful",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+    likes: 5,
+    __v: 0,
+  },
+  {
+    title: "Canonical string reduction",
+    author: "Edsger W. Dijkstra",
+    likes: 12,
+  },
+  {
+    author: "Robert C. Martin",
+    blogs: 3,
+  },
 ];
 
 beforeEach(async () => {
-    await Blog.deleteMany({})
-    for (let item of initial) {
-        let obj = new Blog(item)
-        await obj.save()
-    }
-})
+  await Blog.deleteMany({});
+  for (let item of initial) {
+    let obj = new Blog(item);
+    await obj.save();
+  }
+});
 
+test("a blog can be deleted", async () => {
+  const atStart = await Blog.find({});
+  const toDelete = atStart[0];
 
-test('a blog can be deleted', async () => {
-  const atStart = await Blog.find({})
-  const toDelete = atStart[0]
+  await api.delete(`/api/blogs/${toDelete.id}`).expect(204);
+
+  const atEnd = await Blog.find({});
+
+  expect(atEnd).toHaveLength(initial.length - 1);
+
+  const contents = atEnd.map((r) => r.title);
+
+  expect(contents).not.toContain(toDelete.title);
+});
+
+test("a blog can be updated", async () => {
+  const records = await Blog.find({});
+  const beforeUpdate = records[0].toJSON();
+
+  beforeUpdate.likes = beforeUpdate.likes + 1;
 
   await api
-    .delete(`/api/blogs/${toDelete.id}`)
-    .expect(204)
+    .put(`/api/blogs/${beforeUpdate.id}`)
+    .send(beforeUpdate)
+    .expect(200)
+    .expect("Content-Type", /application\/json/);
 
-  const atEnd = await Blog.find({})
+  const afterUpdated = await Blog.findById(beforeUpdate.id);
 
-  expect(atEnd).toHaveLength(
-    initial.length - 1
-  )
-
-  const contents = atEnd.map(r => r.title)
-
-  expect(contents).not.toContain(toDelete.title)
-})
-
-
-
-
+  expect(afterUpdated.toJSON()).toEqual(beforeUpdate);
+});
 
 afterAll(async () => {
-    await mongoose.connection.close()
-})
+  await mongoose.connection.close();
+});
+
